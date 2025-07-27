c537a2e8cea2d2faba5025489ff6300c9194dda1
debugsoul07 <maheshkumawat0608@gmail.com>
2025-07-27 16:53:58 +0530

    lecture 1

---
+++ src/lecture1/Main.java
@@ -0,0 +1,22 @@
+package lecture1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //     output
+
+        System.out.println("hello world with java");
+
+
+//        System.out.println() is the syntax used to print something in Java.
+//                The println method prints the output and moves the cursor to the next line.
+
+
+
+//         printing the pattern
+        System.out.println("*\n**\n***\n****\n");
+
+    }
+
+}
---
+++ src/lecture1/variables.java
@@ -0,0 +1,10 @@
+package lecture1;
+
+public class variables {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
